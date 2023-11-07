65cd929291fa8403cfbd09ce6accc0cd69c5ca1d
Brad Ito <phlogisticfugu@gmail.com>
2023-11-07 08:13:32 -0800

    upgrade to support python 3.12 but drop 3.7 support. matching uvloop.  also disable curio in 3.12

--- tests/test_curio.py
+++ tests/test_curio.py
@@ -1,10 +1,17 @@
 import asyncio
 
-import curio  # type: ignore
+import pytest
 
 from asyncio_anywhere import asyncio_run
 
+try:
+    import curio  # type: ignore
+    curio_loaded = True
+except ImportError:
+    curio_loaded = False
 
+
+@pytest.mark.skipif(not curio_loaded, reason="curio not installed")
 def test_curio():
     async def nested_asyncio_fn(x):
         await asyncio.sleep(0.001)
